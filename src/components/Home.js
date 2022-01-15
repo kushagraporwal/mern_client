8f3899b1069b8f290f6a7823a106f60e0b3bb418
Kushagra <kushagraporwal2001@gmail.com>
2022-01-16 00:51:21 +0530

    another commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -89,6 +89,7 @@ const Home = () => {
                 credentials: "include"
             });
             const data1= await res.json();
+            window.alert(data1);
             console.log(data1);
             window.location.reload();
             
